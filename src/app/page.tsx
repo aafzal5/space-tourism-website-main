25ecc96f59db047601b57dd365b9d4992454d9c6
simsint <78600926+simsint@users.noreply.github.com>
2023-11-14 17:20:39 +0530

    added responsive bg images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 const Home = () => {
   return (
-    <div className="flex min-h-screen bg-slate-400 flex-col items-center justify-between p-24">
-      Hi there
+    <div className="flex min-h-screen bg-[url('/assets/home/background-home-mobile.jpg')] md:bg-[url('/assets/home/background-home-tablet.jpg')] lg:bg-[url('/assets/home/background-home-desktop.jpg')] font-barlow">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non rerum similique aut consequatur. Aliquam, harum deleniti voluptatem mollitia expedita exercitationem dolores voluptates sunt, libero porro explicabo totam ex vitae aperiam!
     </div>
   );
 };
